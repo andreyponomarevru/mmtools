0ee2b4bce601a33eff6ef83ed8227855edbb7300
Andrey Ponomarev <info@andreyponomarev.ru>
2024-11-13 16:32:59 +0300

    Remove REPORT_NO_COVERS constant

--- src/config.ts
+++ src/config.ts
@@ -11,8 +11,7 @@ export const GENRES = genres;
 
 export const REPORTS_DIR = "./build/reports";
 
-export const REPORT_BAD_COVERS = `${REPORTS_DIR}/small-covers.json`;
-export const REPORT_NO_COVERS = `${REPORTS_DIR}/no-covers.log`;
+export const REPORT_BAD_COVERS = `${REPORTS_DIR}/bad-covers.json`;
 export const REPORT_NO_BPM = `${REPORTS_DIR}/bad-bpm.log`;
 export const REPORT_LOW_BITRATE = `${REPORTS_DIR}/bad-bitrate.log`;
 export const REPORT_BAD_GENRES = `${REPORTS_DIR}/bad-genre.log`;
