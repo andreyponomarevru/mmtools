d51f65dceb373ba1b820c94fccf3a1d0489d4ae2
Andrey Ponomarev <info@andreyponomarev.ru>
2024-12-06 19:07:54 +0300

    Update test

--- __integration-tests__/build-artists-list.test.ts
+++ __integration-tests__/build-artists-list.test.ts
@@ -1,19 +1,21 @@
-import path from "path";
-import util from "util";
-import fs from "fs";
 import { describe, expect, it } from "@jest/globals";
 import { buildArtistsList } from "../src/m3u-to-tracklist/m3u-to-tracklist";
 import { m3uWithAbsolutePaths } from "../src/test-helpers/m3u-playlists";
 
 describe("buildArtistsList", () => {
-  it("given a list of track paths, returns comma-separated list of track artists", async () => {
-    const artists = await buildArtistsList(m3uWithAbsolutePaths.parsed);
+  it("returns comma-separated list of track artists if they are set in ID3 tags", async () => {
+    const m3u = [
+      "./__test-data__/music-lib/invalid-tags/Carlos Nino & Friends - Woo, Acknowledgement.flac",
+      "./__test-data__/music-lib/invalid-tags/11-p__real__albertas--dedicated_2_u-46dd7eff.mp3",
+      "./__test-data__/music-lib/valid-tags/Those Things Deluxe (2007)/CD 2/06 - Miguel Migs - So Far (Rasmus Faber’s Farplane radio edit) [16-44].flac",
+      "./__test-data__/music-lib/invalid-tags/Various - 10/02. The Zenmenn - The Legend Of Haziz.flac",
+    ];
+    const artists = await buildArtistsList(m3u);
 
     expect(artists).toEqual([
       "Carlos Niño & Friends",
       "P. Real",
       "Miguel Migs",
-      "The Zenmenn",
     ]);
   });
 });
