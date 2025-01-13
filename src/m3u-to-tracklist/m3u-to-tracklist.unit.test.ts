3041843d3a1b81f3dfde39468c07bb413678495b
Andrey Ponomarev <info@andreyponomarev.ru>
2025-01-13 04:20:21 +0300

    Add extra metadata (#75)

--- src/m3u-to-tracklist/m3u-to-tracklist.unit.test.ts
+++ src/m3u-to-tracklist/m3u-to-tracklist.unit.test.ts
@@ -1,63 +1,45 @@
 import { UtfString } from "utfstring";
 import { describe, expect, it, jest } from "@jest/globals";
-import { mmFacade } from "../music-metadata-facade";
 import { buildTracklistLine } from "./m3u-to-tracklist";
 
-jest.mock("../music-metadata-facade");
 jest.mock("utfstring");
 
 describe("buildTracklistLine", () => {
   const leftToRightTextMark = "\u200E";
-  const parsedTrack: Awaited<ReturnType<typeof mmFacade.parseFile>> = {
-    meta: {
-      year: 2024,
-      title: "Song Title",
-      artists: ["Artist name 1", "Artist name 2"],
-      album: undefined,
-      genre: undefined,
-      bpm: undefined,
-      label: undefined,
-      catalognumber: undefined,
-      format: undefined,
-      bitrate: undefined,
-      originaldate: undefined,
-    },
-    cover: { format: "", data: Buffer.from([]) },
+  const track = {
+    year: 2024,
+    title: "Song Title",
+    artists: ["Artist name 1", "Artist name 2"],
+    label: "NotYoutBusiness Records",
+    originaldate: "1995",
   };
 
-  it("uses the `originaldate` prop, if it exists", async () => {
-    const track = {
-      ...parsedTrack,
-      meta: { ...parsedTrack.meta, originaldate: "1976" },
-    };
-    jest.mocked(mmFacade).parseFile.mockResolvedValueOnce(track);
-
-    const result = await buildTracklistLine(parsedTrack.meta);
-
-    expect(result).toContain(String(parsedTrack.meta.originaldate));
-  });
-
-  it("uses the `year` prop, if the `originaldate` is undefined", async () => {
-    jest.mocked(mmFacade).parseFile.mockResolvedValueOnce(parsedTrack);
-
-    const result = await buildTracklistLine(parsedTrack.meta);
-
-    expect(result).toContain(String(parsedTrack.meta.year));
-  });
-
-  it("returns the string matching the format `<artists> — <title> (<year>)`, if all required props exist", async () => {
-    jest.mocked(mmFacade).parseFile.mockResolvedValueOnce(parsedTrack);
+  beforeEach(() => {
     jest
       .mocked(UtfString)
-      .mockReturnValueOnce([parsedTrack.meta.artists!.join(", ")] as any)
-      .mockReturnValueOnce([parsedTrack.meta.title] as any);
+      .mockReturnValueOnce([track.artists.join(", ")] as any)
+      .mockReturnValueOnce([track.title] as any);
+  });
 
-    const result = await buildTracklistLine(parsedTrack.meta);
+  it("returns the string matching the format `<artists> — <title> (<original date>/<year>, <label>)`, if all required props exist", async () => {
+    const result = buildTracklistLine(track);
 
     expect(result).toBe(
-      `${parsedTrack.meta.artists![0]}, ${parsedTrack.meta.artists![1]} — ${
-        parsedTrack.meta.title
-      } (${leftToRightTextMark}${parsedTrack.meta.year})`
+      `${track.artists![0]}, ${track.artists![1]} — ${
+        track.title
+      } (${leftToRightTextMark}${track.originaldate}/${track.year}, ${
+        track.label
+      })`
+    );
+  });
+
+  it("returns the string matching the format `<artists> — <title> (<year>, <label>)`, if 'originaldate' prop doesn't exist", async () => {
+    const result = buildTracklistLine({ ...track, originaldate: undefined });
+
+    expect(result).toBe(
+      `${track.artists![0]}, ${track.artists![1]} — ${
+        track.title
+      } (${leftToRightTextMark}${track.year}, ${track.label})`
     );
   });
 });
