34ea5baf18a1da806a41abb5347ffdca862715bb
Andrey Ponomarev <info@andreyponomarev.ru>
2025-01-14 06:00:11 +0300

    Temp results

--- src/build-cover/index.ts
+++ src/build-cover/index.ts
@@ -1,10 +1,16 @@
 import { init } from "./init";
 
 const m3uPathArg = process.argv[2];
+const photoPathArg = process.argv[3];
+const date = process.argv[4];
 
-if (!m3uPathArg) {
-  console.log("\nPlease, provide m3u playlist path\n");
+if (!m3uPathArg || !photoPathArg || !date) {
+  console.log(
+    "\nPlease, provide arguments in the following format:\n\n    <m3u playlist path> <photo path> <date>\n"
+  );
   process.exit(1);
 }
 
-init(m3uPathArg).catch(console.error);
+init({ m3uPath: m3uPathArg, photoPath: photoPathArg, date }).catch(
+  console.error
+);
--- src/build-cover/init.ts
+++ src/build-cover/init.ts
@@ -1,9 +1,30 @@
 import fs from "fs";
+import { exec, ExecException } from "child_process";
 import { processBrokenM3Upaths } from "../utils";
 import { validateM3UfilePaths } from "../utils";
+import { EXTRACTED_COVERS_DIR } from "../config/constants";
+import { init as extractCovers } from "../extract-covers/init";
 
-export async function init(m3uFilePath: string) {
-  const paths = await validateM3UfilePaths(m3uFilePath);
+type Init = { m3uPath: string; photoPath: string; date: string };
+
+const callback = (
+  err: ExecException | null,
+  stdout: string,
+  stderr: string
+) => {
+  console.log(stdout);
+
+  if (err !== null) console.error(err);
+};
+
+export async function init({ m3uPath, photoPath, date }: Init) {
+  const paths = await validateM3UfilePaths(m3uPath);
   await processBrokenM3Upaths(paths.broken);
   await fs.promises.writeFile("./build/track-paths.txt", paths.ok.join("\n"));
+
+  await extractCovers(m3uPath);
+
+  const CMD = `bash "./src/build-cover/build" "${photoPath}" "${date}" "./build/track-paths.txt" "${EXTRACTED_COVERS_DIR}"`;
+
+  exec(CMD, callback);
 }
