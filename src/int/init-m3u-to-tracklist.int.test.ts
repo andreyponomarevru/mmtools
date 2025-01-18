80a2e3352be796935731ebb336e5578f43e97f49
Andrey Ponomarev <info@andreyponomarev.ru>
2025-01-18 04:59:02 +0300

    Fix

--- src/int/init-m3u-to-tracklist.int.test.ts
+++ src/int/init-m3u-to-tracklist.int.test.ts
@@ -15,8 +15,8 @@ describe("converts m3u into tracklist", () => {
   beforeEach(async () => await clearDir(BUILD_DIR));
 
   it.only("throws an error on validation error if 'shouldThrow' arg is set to true", async () => {
-    jest.spyOn(console, "error").mockImplementation(jest.fn());
-    jest.spyOn(process, "exit").mockImplementation(jest.fn() as any);
+    //jest.spyOn(console, "error").mockImplementation(jest.fn());
+    //jest.spyOn(process, "exit").mockImplementation(jest.fn() as any);
 
     await expect(init(M3U_PATH, true)).rejects.toThrowError(
       "MISSING ID3 TAGS. See logs in /build dir"
